ef7fd1d2de626e23b2c8881e1ad3a28075257b2a
ThatUserCalledDevin <hershey1@etown.edu>
2025-07-31 12:46:15 -0400

    adding devin.php I think

---
+++ about/devin.php
@@ -0,0 +1,27 @@
+<?php
+$pageName = "Devin's Webpage";
+require "../includes/functions.php";
+require "../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Devin's Bio">
+            <div class="bio-title">Jane Doe</div>
+            <br>
+            <div id="bio"> Hey all, Devin here! I have been asked across many years to make bios about myself, 
+                and I rarely know what to write in them.
+        </section>
+    </main>
+</body>
+<?php
+     require "../includes/footer.php";
+?>
