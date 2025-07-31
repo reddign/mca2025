28f6f77f6a7e0c19c9e2923ab18475f97e469944
Jaylaines <delgado@etown.edu>
2025-07-31 12:47:15 -0400

    added my page

---
+++ about/jayla.php
@@ -0,0 +1,26 @@
+<?php
+$pageName = "Jayla's Webpage";
+require "../includes/functions.php";
+require "../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Jane Doe's Bio">
+            <div class="bio-title">Jane Doe</div>
+            <br>
+            <div id="bio"> My name is Jayla! This academy was my first introduction to coding. I heart vanilla coke!
+        </section>
+    </main>
+</body>
+<?php
+     require "../includes/footer.php";
+?>
