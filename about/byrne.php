54f8602821de53b689a8133c54b6c589456480cc
GoobertonTheThird <byrne@etown.edu>
2025-07-31 12:47:05 -0400

    I've added my webpage called byrne.php

---
+++ about/byrne.php
@@ -0,0 +1,26 @@
+<?php
+$pageName = "Ethan Byrne's Webpage";
+require "../includes/functions.php";
+require "../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Ethan Byrne's Bio">
+            <div class="bio-title">Ethan Byrne</div>
+            <br>
+            <div id="bio"> I am Ethan. I think therefore I am.
+        </section>
+    </main>
+</body>
+<?php
+     require "../includes/footer.php";
+?>
