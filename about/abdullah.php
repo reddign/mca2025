1ec6019cf7a4b7e816fd4f839f979f99ced1a647
Abd-ten <TANIS@etown.edu>
2025-07-31 12:47:57 -0400

    added my web page

---
+++ about/abdullah.php
@@ -0,0 +1,29 @@
+<?php
+$pageName = "Abdullahs' Webpage";
+require "../includes/functions.php";
+require "../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Abdullahs Bio">
+            <div class="bio-title">Abdullah Tanis</div>
+            <br>
+            <div id="bio"> Abdullah is a student in this class. He's a senior at manheim township High School.
+                He doesn't know what he wants to major in, or even if he wants to go to college at all. 
+                If he does go to a college. he'd like for it to be Millersville.
+                If he does go to Millersville, he's considering getting a major in bio-chemistry 
+        </section>
+    </main>
+</body>
+<?php
+     require "../includes/footer.php";
+?>
