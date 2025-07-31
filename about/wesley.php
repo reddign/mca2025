3beed2d14ff01f9a4eda6272cbf502be3784b685
Wesley Groff <groff1@etown.edu>
2025-07-31 12:46:03 -0400

    Added my web page

---
+++ about/wesley.php
@@ -0,0 +1,26 @@
+<?php
+$pageName = "Wesley Groff's Webpage";
+require "../includes/functions.php";
+require "../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Wesley Groff's Bio">
+            <div class="bio-title">Wesley Groff</div>
+            <br>
+            <div id="bio"> Wesley Groff is a student for Momentum Coding Academy 2025.
+        </section>
+    </main>
+</body>
+<?php
+     require "../includes/footer.php";
+?>
