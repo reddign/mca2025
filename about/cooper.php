233e4cadaf6eca73dc50a9ed3e1c541863be9d7f
Cooper Lehman <LEHMAN@etown.edu>
2025-07-31 12:48:57 -0400

    added bio

---
+++ about/cooper.php
@@ -0,0 +1,28 @@
+<?php
+$pageName = "Cooper's Webpage";
+require "../includes/functions.php";
+require "../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Cooper's Bio">
+            <div class="bio-title">Cooper</div>
+            <br>
+            <div id="bio"> Matt is a student in this class. He is also a student at Conestoga Valley High School. 
+                Matt is going into his junior year. Will he pass his classes? Time will tell. Matt is not short for Matthew, it 
+                is actually short for J Matt, which is short for Just Matt. 
+        </section>
+    </main>
+</body>
+<?php
+     require "../includes/footer.php";
+?>
