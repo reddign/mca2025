ef60ec8c902bd45f8607b9955243c934c175fab9
Evan Harner <HARNER@etown.edu>
2025-07-31 12:46:03 -0400

    added bio

---
+++ about/evan.php
@@ -0,0 +1,27 @@
+<?php
+$pageName = "Evan Harner's Webpage";
+require "../includes/functions.php";
+require "../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Jane Doe's Bio">
+            <div class="bio-title">Evan Harner</div>
+            <br>
+            <div id="bio"> Evan is a student at Conestoga Valley High School. He enjoys STEM classes at school and has been involved in several
+                clubs such as TSA. 
+        </section>
+    </main>
+</body>
+<?php
+     require "../includes/footer.php";
+?>
