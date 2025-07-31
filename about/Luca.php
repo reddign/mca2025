ccf09da2c9ac0dbad82c1455d22e459a18c409e6
TStarfall-Luca <suliveras@etown.edu>
2025-07-31 12:52:48 -0400

    added my webpage

---
+++ about/Luca.php
@@ -0,0 +1,29 @@
+<?php
+$pageName = "Luca Suliveras's Webpage";
+require "../includes/functions.php";
+require "../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Luca's Bio">
+            <div class="bio-title">Jane Doe</div>
+            <br>
+            <div id="bio"> Luca J.M. Suliveras is a student at Momentum Coding Academy. He is a rising senior in high school, and he is 
+                thinking about a wide variety of career paths, most notably Computer Science with a minor in Visual Art, or Art 
+                History, with a minor in Psychology. He intends to graduate high school in 2026, and wants to pursue further 
+                education at either Duquesne University, or UPitt.
+        </section>
+    </main>
+</body>
+<?php
+     require "../includes/footer.php";
+?>
