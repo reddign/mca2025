b479be1f45cc864ffb2a36070188ae48bdd5a204
Nathan Andemichael <andemichael@etown.edu>
2025-07-31 13:36:32 -0400

    edited my pageee

--- about/nathana.php
+++ about/nathana.php
@@ -17,9 +17,7 @@ require "../includes/head.php";
         <section id="Nathan Andemichael's Bio">
             <div class="bio-title">Nathan Andemichael</div>
             <br>
-            <div id="bio"> Nathan is a student in this class. If she was she might be a junior or senior at Somewhere HS.
-                She might be thinking about majoring in Computer Science, with a concentration in Web and Application 
-                Design, plus a minor in Graphic Design, and intends to graduate in 2030 or 2031 from Xtown College or University.
+            <div id="bio"> Nathan is a student of Momentum Coding Academy and has no interest in computer science.
         </section>
     </main>
 </body>
