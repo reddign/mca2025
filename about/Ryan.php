a9192a0ab26286938af2e37778be6180922f0845
SF-rj <Lefever@etown.edu>
2025-07-31 09:46:12 -0700

    I'm adding my page.

---
+++ about/Ryan.php
@@ -0,0 +1,26 @@
+<?php
+$pageName = "Ryan's Webpage";
+require "../includes/functions.php";
+require "../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Jane Doe's Bio">
+            <div class="bio-title">Ryan</div>
+            <br>
+            <div id="bio"> My name is Ryan, and I'm a student of the momentum class. I'm going to be a rising senior, and I wanna major in computer science in college.
+        </section>
+    </main>
+</body>
+<?php
+     require "../includes/footer.php";
+?>
