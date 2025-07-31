942189ff55340c636079e14f56b44b7574d953a0
Henry Sokra <Sokra@etown.edu>
2025-07-31 12:44:57 -0600

    That sure is a web page.

---
+++ about/Henry.php
@@ -0,0 +1,27 @@
+<?php
+$pageName = "Henry's Webpage";
+require "../includes/functions.php";
+require "../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Henry's Bio">
+            <div class="bio-title">Henry Sokra</div>
+            <br>
+            <div id="bio"> Henry is a student in this class. He's a rising junior. He's 
+            probably going to go to Millersville in the future, though things could change. 
+        </section>
+    </main>
+</body>
+<?php
+     require "../includes/footer.php";
+?>
