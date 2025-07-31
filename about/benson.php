d372147ea4e2d92d18aa85078a88215b12dae678
Benson <liu@etown.edu>
2025-07-31 12:46:26 -0400

    added my web page

---
+++ about/benson.php
@@ -0,0 +1,27 @@
+<?php
+$pageName = "Benson's Webpage";
+require "../includes/functions.php";
+require "../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Benson's Bio">
+            <div class="bio-title">Benson</div>
+            <br>
+            <div id="bio"> Benson is a member of the Momentum Coding Academy. He is a rising senior who is interested in majoring in Computer Science. 
+                Some of Benson's interests includes listening to music, playing video games, and coding. Ultimately, he intends to graduate in 2030 from a college or university.
+        </section>
+    </main>
+</body>
+<?php
+     require "../includes/footer.php";
+?>
