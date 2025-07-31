dd8713b25575a4ce32ca78992214ca2805d14dde
Brian Shore <shore@etown.edu>
2025-07-31 09:52:33 -0700

    brian.php

---
+++ about/brian.php
@@ -0,0 +1,26 @@
+<?php
+$pageName = "Brian Shore's Webpage";
+require "../includes/functions.php";
+require "../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Brian Shore's Bio">
+            <div class="bio-title">Jane Doe</div>
+            <br>
+            <div id="bio"> I am a current student in the Momentum Coding Academy.
+        </section>
+    </main>
+</body>
+<?php
+     require "../includes/footer.php";
+?>
