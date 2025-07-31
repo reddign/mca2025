c7aa4eb0440602a9a9993e1cc84d652696b5762a
Mildred <nwachukwu.mildred10@gmail.com>
2025-07-31 12:46:01 -0400

    added my web page

---
+++ about/mildred.php
@@ -0,0 +1,26 @@
+<?php
+$pageName = "Mildred's Webpage";
+require "../includes/functions.php";
+require "../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Mildred's Bio">
+            <div class="bio-title">Mildred</div>
+            <br>
+            <div id="bio"> Mildred is an Etown student and is a TA for MCA. 
+        </section>
+    </main>
+</body>
+<?php
+     require "../includes/footer.php";
+?>
