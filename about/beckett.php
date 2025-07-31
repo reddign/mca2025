7c449233647d667b0d4fcd40abcb2a9d4534fa09
Beckett Mann <mann@etown.edu>
2025-07-31 13:00:28 -0600

    beckett's webpage

---
+++ about/beckett.php
@@ -0,0 +1,28 @@
+<?php
+$pageName = "Beckett's Webpage";
+require "../includes/functions.php";
+require "../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Beckett's Bio">
+            <div class="bio-title">Beckett</div>
+            <br>
+            <div id="bio"> Beckett is not a student in this class. If she was she might be a junior or senior at Somewhere HS.
+                She might be thinking about majoring in Computer Science, with a concentration in Web and Application 
+                Design, plus a minor in Graphic Design, and intends to graduate in 2030 or 2031 from Xtown College or University.
+        </section>
+    </main>
+</body>
+<?php
+     require "../includes/footer.php";
+?>
