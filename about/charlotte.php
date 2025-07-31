38179b7dcb993c8266050a80bb8ee78e87bed991
Charlotte Cohick <cohick@etown.edu>
2025-07-31 12:46:47 -0400

    added my page

---
+++ about/charlotte.php
@@ -0,0 +1,28 @@
+<?php
+$pageName = "Charlotte's Webpage";
+require "../includes/functions.php";
+require "../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Charlotte's Bio">
+            <div class="bio-title">Jane Doe</div>
+            <br>
+            <div id="bio"> CHarlotte is an upcoming junior at Penn Manor Highschool. She is planning to major in forensic 
+                anthropology and graduate in 2031 from The University of Tennesse. 
+                
+        </section>
+    </main>
+</body>
+<?php
+     require "../includes/footer.php";
+?>
