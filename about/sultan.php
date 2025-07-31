54a19df1101c56bb0627a21b1284354af3baf26f
Abdullah Sultan <sultan@etown.edu>
2025-07-31 12:46:28 -0400

    added my webpage

---
+++ about/sultan.php
@@ -0,0 +1,28 @@
+<?php
+$pageName = "Abdullah Sultan's Webpage";
+require "../includes/functions.php";
+require "../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Abdullah Sultan's Bio">
+            <div class="bio-title">Jane Doe</div>
+            <br>
+            <div id="bio"> Abdullah Sultan is a student in Momentum Coding and academy. He is a rising senior in Manheim Township High School
+                He is thinking about majoring in potentially either computer science or aerospace engineering.
+            
+        </section>
+    </main>
+</body>
+<?php
+     require "../includes/footer.php";
+?>
