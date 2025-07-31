63b6e557e8b643da55210a08f9b4570db03de6aa
byentirc <castillohuitzil@etown.edu>
2025-07-31 13:06:20 -0400

    added my page

---
+++ about/britney.php
@@ -0,0 +1,26 @@
+<?php
+$pageName = "Britney Castillo's Webpage";
+require "../includes/functions.php";
+require "../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Britney CAstillo's Bio">
+            <div class="bio-title">Britney Castillo</div>
+            <br>
+            <div id="bio"> Britney Castillo is an incoming junior at JP Mccaskey.
+        </section>
+    </main>
+</body>
+<?php
+     require "../includes/footer.php";
+?>
