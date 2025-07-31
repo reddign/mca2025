c6bda52bba7e27e521ff7ffe443e2db7a6097a40
Henry Sokra <Sokra@etown.edu>
2025-07-31 13:36:03 -0600

    Link added in my name

--- about/MCA.php
+++ about/MCA.php
@@ -47,7 +47,7 @@ Selena Monk,
 <a href='Gabriel.php'>Gabriel Perkins,</a> 
 Orlando Santiago, 
 Brian Shore, 
-Henry Sokra, 
+<a href="Henry.php">Henry Sokra,</a> 
 <a href='daniel.php'>Daniel Spooner,</a>
 Luca Suliveras, 
 Abdullah Sultan,
