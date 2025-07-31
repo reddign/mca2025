75eb0c67c48eb1dc70b2b64cfc96522b7c976b14
cruz1MCA <cruz1@etown.edu>
2025-07-31 12:46:33 -0400

    added my web page

---
+++ about/micah.php
@@ -0,0 +1,26 @@
+<?php
+$pageName = "Micah's Webpage";
+require "../includes/functions.php";
+require "../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Jane Doe's Bio">
+            <div class="bio-title">Micah</div>
+            <br>
+            <div id="bio"> Hi! I'm a student in Momentum Coding Academy and a rising 11th grader at Penn Manor High School.
+        </section>
+    </main>
+</body>
+<?php
+     require "../includes/footer.php";
+?>
