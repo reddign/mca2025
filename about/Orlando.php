9b86db172c95273c70c6416db41f648b16472421
Orlando <santiago@etown.edu>
2025-07-31 12:57:30 -0400

    adding my web page

---
+++ about/Orlando.php
@@ -0,0 +1,27 @@
+<?php
+$pageName = "Jane Doe's Webpage";
+require "../includes/functions.php";
+require "../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Jane Doe's Bio">
+            <div class="bio-title">Orlando Santiago</div>
+            <br>
+            <div id="bio"> I'm a rising senior attending MCA and coming from McCaskey.I'm thinking about majoring in Computer Science, with a concentration in Web and Application 
+                Design, plus a minor in Graphic Design, and intends to graduate in 2030 or 2031 from etown College or University. Plan to at least
+        </section>
+    </main>
+</body>
+<?php
+     require "../includes/footer.php";
+?>
