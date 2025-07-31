77882d23491da1f2188b3b62067d0fbf5c76acf7
PieSavage <Perkins@etown.edu>
2025-07-31 12:46:11 -0400

    added my page

---
+++ about/Gabriel.php
@@ -0,0 +1,28 @@
+<?php
+$pageName = "Gabriel Perkins's Webpage";
+require "../includes/functions.php";
+require "../includes/head.php";
+
+?>
+<body>
+<?php
+     require "../includes/navbar.php";
+?>
+   
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+        
+    <main>
+        <section id="Gabriel Perkins's Bio">
+            <div class="bio-title">Gabriel</div>
+            <br>
+            <div id="bio"> Gabriel Perkins is a student in this class. He is a junior at Penn Manor HS.
+                He is thinking about majoring in Computer Science, with a concentration in game 
+                Design, and doesn't know whether or not he will go to college.
+        </section>
+    </main>
+</body>
+<?php
+     require "../includes/footer.php";
+?>
